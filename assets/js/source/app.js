c15147824f2d78ed4e5a885d2fbc92832383f640
joshdaugherty <joshdaugherty@gmail.com>
2019-05-14 14:39:28 -0500

    Remove old search and quick links JS
    The updated JS is in standalone files and enqueued

--- assets/js/source/app.js
+++ assets/js/source/app.js
@@ -3,28 +3,4 @@
 		$('.gallery').find('br').detach();
 	});
 
-
-	$(document).ready(function($) { 
-	// Show/hide Search Box
-		
-		$(".toggle-search").click(function() {
-				$(".search-wrap").slideToggle('fast', function(){
-					$(".toggle-search").toggleClass('active');
-					$("#toggle-search .google-search input[type=text]").focus();
-				});
-				return false;
-			
-		});
-
-		$('#sidebarCollapse').on('click', function () {
-			$('#sidebar').toggleClass('active');
-			$('.site-container').toggleClass('active');
-			$('#sidebarCollapse .fa-bars').toggleClass('active');
-			$('#sidebarCollapse .fa-times').toggleClass('active');
-			$('.collapse.in').toggleClass('in');
-			$('#sidebarCollapse[aria-expanded=true]').attr('aria-expanded', 'false');
-		});
-
-	});
-
 })(jQuery);
