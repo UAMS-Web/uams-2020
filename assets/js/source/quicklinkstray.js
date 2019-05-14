e44e01b1181659ee789625a3459455cf5a5e51e2
joshdaugherty <joshdaugherty@gmail.com>
2019-05-14 14:37:34 -0500

    Expand quick links toggle functionality and account for new close button inside quick links tray

--- assets/js/source/quicklinkstray.js
+++ assets/js/source/quicklinkstray.js
@@ -1,7 +1,18 @@
 jQuery( document ).ready( function( $ ) {
     quicklinksvisible = false;
-    $(".quick-links-toggler").click(function() {
+
+    $("#toggle-quick-links, #toggle-quick-links-inside").click(function() {
         $("#quick-links").toggleClass("closed");
+        $("#site-container-outer").toggleClass("quick-links-active");
+
+        if (quicklinksvisible) {
+            $("#toggle-quick-links, #toggle-quick-links-inside").attr("aria-expanded", false);
+            $("#toggle-quick-links .label, #toggle-quick-links-inside .label").text("Expand Quick Links");
+        } else {
+            $("#toggle-quick-links, #toggle-quick-links-inside").attr("aria-expanded", true);
+            $("#toggle-quick-links .label, #toggle-quick-links-inside .label").text("Collapse Quick Links");
+        }
+
         quicklinksvisible = !quicklinksvisible;
     });
 } );
