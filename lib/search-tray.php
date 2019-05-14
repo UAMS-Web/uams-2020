84a897d32ae162b6fa29f41b7e2c59f967bc4f6f
joshdaugherty <joshdaugherty@gmail.com>
2019-05-14 14:36:18 -0500

    Add close button inside search tray

--- lib/search-tray.php
+++ lib/search-tray.php
@@ -5,6 +5,10 @@ add_action('genesis_before', 'uams_toggle_search', 4);
 function uams_toggle_search(){
  echo '<div id="header-search" class="closed">' . "\n";
  echo '<div class="inner-container">' . "\n";
+ echo '<button type="button" id="toggle-search-inside" aria-controls="header-search" aria-expanded="false" aria-label="Toggle Search">' . "\n";
+ echo '<span class="sr-only label">Expand Search</span>' . "\n";
+ echo '<span class="fas fa-times fa-lg fa-fw"></span>' . "\n";
+ echo '</button>' . "\n";
 
  echo get_search_form();
 
