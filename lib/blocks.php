97bb5f06bf935d751abbc0924de57dd390f722e5
joshdaugherty <joshdaugherty@gmail.com>
2019-06-11 13:51:12 -0500

    Require link list item link URL in side-by-side block

--- lib/blocks.php
+++ lib/blocks.php
@@ -210,7 +210,7 @@ if( function_exists('acf_add_local_field_group') ):
                         'name' => 'side_image_link_list_url',
                         'type' => 'url',
                         'instructions' => '',
-                        'required' => 0,
+                        'required' => 1,
                         'conditional_logic' => 0,
                         'wrapper' => array(
                             'width' => '',
