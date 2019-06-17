06070c4f2073616b93dbd9e98ccae89d5252af0f
todd-uams <todd@uams.edu>
2019-06-17 10:58:04 -0500

    Add news block

--- lib/misc.php
+++ lib/misc.php
@@ -168,6 +168,7 @@ function uamswp_allowed_block_types( $allowed_blocks ) {
 		'core-embed/scribd',
 		'core-embed/slideshare',
 		'core-embed/ted',
+		'uamswp/uams-news', // If available
 	), $registered_blocks );
  
 }
