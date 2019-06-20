9bd404e7995489af4057291b29e0225e68db8dff
todd-uams <todd@uams.edu>
2019-06-20 08:58:10 -0500

    Remove "home-featured" widget area
    It uses the jumbotron bootstrap section

--- lib/widget-areas.php
+++ lib/widget-areas.php
@@ -12,15 +12,15 @@
 */
 
 // Register Sidebar Function
-add_action( 'init', 'uamswp_register_sidebars' );
-function uamswp_register_sidebars() {
-	// Register Custom Sidebars
-	genesis_register_sidebar( array(
-		'id' => 'home-featured',
-		'name' => __( 'Home Featured', 'uams-2020' ),
-		'description' => __( 'This is the home featured area. It uses the jumbotron bootstrap section.', 'uams-2020' )
-	) );
-}
+// add_action( 'init', 'uamswp_register_sidebars' );
+// function uamswp_register_sidebars() {
+// 	// Register Custom Sidebars
+// 	genesis_register_sidebar( array(
+// 		'id' => 'home-featured',
+// 		'name' => __( 'Home Featured', 'uams-2020' ),
+// 		'description' => __( 'This is the home featured area. It uses the jumbotron bootstrap section.', 'uams-2020' )
+// 	) );
+// }
 
 //* Unregister primary sidebar
 // unregister_sidebar( 'sidebar' );
