5100886cfc9b00c61e8bfcf7a76fcf5edcfc69a2
todd-uams <todd@uams.edu>
2019-06-20 08:59:56 -0500

    Remove "Footer 2" and "Footer 3" widgets

--- lib/widget-areas.php
+++ lib/widget-areas.php
@@ -26,3 +26,7 @@
 // unregister_sidebar( 'sidebar' );
 //* Unregister secondary sidebar
 unregister_sidebar( 'sidebar-alt' );
+//* Unregister Footer 2 sidebar
+unregister_sidebar( 'footer-2' );
+//* Unregister Footer 3 sidebar
+unregister_sidebar( 'footer-3' );
