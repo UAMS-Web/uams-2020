5fedc355823f13c70ab934f2befa873b57bc2e40
joshdaugherty <joshdaugherty@gmail.com>
2019-07-10 09:02:34 -0500

    Fix typo

--- lib/page.php
+++ lib/page.php
@@ -15,7 +15,7 @@ function page_options() {
             add_filter('genesis_attr_entry-header', 'uamswp_attributes_entry_header');
         } elseif ('graphic' == $pageTitle) {
             // Graphic Header
-            // Romve original
+            // Remove original
             remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
             remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
             remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
