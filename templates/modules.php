f86d9bd2131c48fc53eb36010255a5e5cab5b518
todd-uams <todd@uams.edu>
2019-07-17 09:49:48 -0500

    Add article element for consistency

--- templates/modules.php
+++ templates/modules.php
@@ -35,6 +35,8 @@ add_filter( 'genesis_attr_site-inner', 'be_site_inner_attr' );
 // Build the page
 get_header();
 echo '<div class="content-sidebar-wrap row"><main class="content col-sm-12" id="genesis-content">';
+echo '<article class="page type-page modules-layout" itemscope itemtype="https://schema.org/CreativeWork">';
 uamswp_modules();
+echo '</article>';
 echo '</main></div>';
 get_footer();
