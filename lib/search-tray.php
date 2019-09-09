bcd7931476913ed178c95f89a6d7fa3218893762
joshdaugherty <joshdaugherty@gmail.com>
2019-09-09 14:21:32 -0500

    Update search tray toggle icon and text for accessibility

--- lib/search-tray.php
+++ lib/search-tray.php
@@ -5,9 +5,9 @@ add_action('genesis_before', 'uams_toggle_search', 4);
 function uams_toggle_search(){
  echo '<div id="header-search" class="closed">' . "\n";
  echo '<div class="inner-container">' . "\n";
- echo '<button type="button" id="toggle-search-inside" aria-controls="header-search" aria-expanded="false" aria-label="Toggle Search">' . "\n";
- echo '<span class="sr-only label">Expand Search</span>' . "\n";
- echo '<span class="fas fa-times fa-lg fa-fw"></span>' . "\n";
+ echo '<button type="button" id="toggle-search-inside" aria-controls="header-search" aria-expanded="false" title="Close Search">' . "\n";
+ echo '<span class="sr-only label">Toggle Search</span>' . "\n";
+ echo '<span class="fas fa-times fa-lg fa-fw" aria-hidden="true"></span>' . "\n";
  echo '</button>' . "\n";
 
  echo get_search_form();
