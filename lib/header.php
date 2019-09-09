8d784fdb0a4b6f237bc8801d94ee60e545664138
joshdaugherty <joshdaugherty@gmail.com>
2019-09-09 14:26:40 -0500

    Update mobile header nav toggle icon and text for accessibility

--- lib/header.php
+++ lib/header.php
@@ -110,9 +110,9 @@ function ursidae_site_image() {
 		</button>
 
 		<!-- // The data-target and aria-controls may need to be dynamically defined. -->
-		<button class="navbar-toggler mobile-menu-toggler" type="button" data-toggle="collapse" data-target="#nav-primary" aria-controls="nav-primary" aria-expanded="false" aria-label="Toggle Primary navigation">
-			<span class="sr-only label">Expand Primary Nav</span>
-			<span class="fas fa-bars fa-lg fa-fw"></span>
+		<button class="navbar-toggler mobile-menu-toggler" type="button" data-toggle="collapse" data-target="#nav-primary" aria-controls="nav-primary" aria-expanded="false" title="Toggle Primary navigation">
+			<span class="sr-only label">Toggle Primary Nav</span>
+			<span class="fas fa-bars fa-lg fa-fw" aria-hidden="true"></span>
 		</button>
 	
 	</nav>
