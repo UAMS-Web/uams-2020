9949387aad8a3f272ba58a4b2b264d4586a09996
joshdaugherty <joshdaugherty@gmail.com>
2019-09-10 12:16:44 -0500

    Set favicon version query strings for cache-busting

--- lib/favicon.php
+++ lib/favicon.php
@@ -6,15 +6,15 @@
 
 function uamswp_add_favicon() { ?>
     <?php // generics ?>
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-128x128.png" sizes="128x128">
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-167x167.png" sizes="167x167">
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-192x192.png" sizes="192x192">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-32x32.png?v=1" sizes="32x32">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-96x96.png?v=1" sizes="96x96">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-128x128.png?v=1" sizes="128x128">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-167x167.png?v=1" sizes="167x167">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-192x192.png?v=1" sizes="192x192">
     <?php // iOS ?>
-    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-120x120.png" sizes="120x120">
-    <link rel="apple-touch-icon" href="path/to/favicon-152x152.png" sizes="152x152">
-    <link rel="apple-touch-icon" href="path/to/favicon-180x180.png" sizes="180x180">
+    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-120x120.png?v=1" sizes="120x120">
+    <link rel="apple-touch-icon" href="path/to/favicon-152x152.png?v=1" sizes="152x152">
+    <link rel="apple-touch-icon" href="path/to/favicon-180x180.png?v=1" sizes="180x180">
     <?php // Windows 8 IE 10 ?>
     <meta name="msapplication-TileColor" content="#9D2235">
     <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon/favicon-144x144.png">
