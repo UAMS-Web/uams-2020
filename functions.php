2ceef1217553fce67f3452eedab7903cd8f0d878
todd-uams <todd@uams.edu>
2020-01-15 12:12:31 -0600

    Header validation fixes
    Remove
    - REST API link tag
    - oEmbed Discovery link
    - REST API  link in header

--- functions.php
+++ functions.php
@@ -36,6 +36,13 @@ function uamswp_childtheme_setup() {
 	remove_action( 'wp_head', 'adjacent_posts_rel_link' );
 	remove_action( 'wp_head', 'wp_shortlink_wp_head' );
 
+	// Disable REST API link tag
+	remove_action('wp_head', 'rest_output_link_wp_head', 10);
+	// Disable oEmbed Discovery Links
+	remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+	// Disable REST API link in HTTP headers
+	remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
 	// Add HTML5 markup structure
 	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
