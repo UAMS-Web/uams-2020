54255f8ddebd6c4642a6db9ee3a75600d9736a97
todd-uams <todd@uams.edu>
2020-02-12 11:42:14 -0600

    Sanitize title

--- lib/a11y.php
+++ lib/a11y.php
@@ -43,9 +43,9 @@ function filter_oembed_dataparse( $return, $data, $url ) {
         * Otherwise, add the title attribute.
         */
     if ( $has_title_attr ) {
-        $return = preg_replace( $preg_match, 'title="' . $title . '"', $return );
+        $return = preg_replace( $preg_match, 'title="' . esc_attr($title) . '"', $return );
     } else {
-        $return = preg_replace( '/^\<iframe/i', '<iframe title="' . $title . '"', $return );
+        $return = preg_replace( '/^\<iframe/i', '<iframe title="' . esc_attr($title) . '"', $return );
     }
 
     $return = str_replace( 'frameborder="0"', '', $return ); // Quick strip of frameborder
