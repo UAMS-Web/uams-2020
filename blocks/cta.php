edaf70b27d1abb30452edc3a8aeb6dbca6f1eea4
joshdaugherty <joshdaugherty@gmail.com>
2020-03-12 13:30:11 -0500

    Add no-link class to container if no link

--- blocks/cta.php
+++ blocks/cta.php
@@ -54,7 +54,7 @@ if ( $background_color == 'bg-white' || $background_color == 'bg-gray' ) {
 }
 
 ?>
-<section class="uams-module cta-bar <?php echo $className; ?> <?php echo $layout; ?> <?php echo $background_color; ?><?php echo $use_image ? ' bg-image' : ''; ?>" id="<?php echo $id; ?>" aria-label="<?php echo $heading; ?>">
+<section class="uams-module cta-bar <?php echo $className; ?> <?php echo $layout; ?> <?php echo $background_color; ?><?php echo $use_image ? ' bg-image' : ''; ?><?php echo $button_url ? '' : ' no-link'; ?>" id="<?php echo $id; ?>" aria-label="<?php echo $heading; ?>">
 <?php if ( $use_image && function_exists( 'fly_add_image_size' ) ) { ?>
     <style>
         #<?php echo $id; ?>:before {
