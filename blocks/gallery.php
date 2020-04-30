f41672759ec3b6c10b08e178f3b6c6d957c26564
Josh <joshdaugherty@gmail.com>
2020-04-30 15:21:06 -0500

    Adjust image container structure to use padded-grid utility class

--- blocks/gallery.php
+++ blocks/gallery.php
@@ -65,38 +65,40 @@ if ($gallery_columns == '2') {
                 <?php echo $description ? '<div class="module-body">'. $description .'</div>' : ''; ?>
             </div>
         </div>
-        <div class="row image-container">
-            <?php 
-                $i=0;
-                foreach($gallery_images as $gallery_image) {
-                // Load values.
-                $image_url = $gallery_image['url'];
-                $image_alt = $gallery_image['alt'];
-                $image_md_url = $gallery_image['sizes']['aspect-16-9'];
-                $image_id = $gallery_image['id'];
-                /* <img class="w-100" src="<?php echo esc_url($image_url); ?>" alt="<?php echo $image_alt; ?>"> */
-                ?>
-                        <div class="col-12 col-sm-<?php echo $sm ?> col-lg-<?php echo $lg; ?> pb-6">
-                            <a href="#" data-toggle="modal" data-target="#modal_<?php echo $i; ?>_<?php echo $id; ?>"><?php echo wp_get_attachment_image($image_id, 'full', "", array( "class" => "w-100" )) ?></a>
-                        </div>
-                        <div class="modal fade" id="modal_<?php echo $i; ?>_<?php echo $id; ?>" tabindex="-1" role="dialog" aria-label="<?php echo $image_alt; ?>" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img class="w-100" src="<?php echo esc_url($image_url); ?>" alt="<?php echo $image_alt; ?>">
+        <div class="image-container padded-grid">
+            <div class="row">
+                <?php 
+                    $i=0;
+                    foreach($gallery_images as $gallery_image) {
+                    // Load values.
+                    $image_url = $gallery_image['url'];
+                    $image_alt = $gallery_image['alt'];
+                    $image_md_url = $gallery_image['sizes']['aspect-16-9'];
+                    $image_id = $gallery_image['id'];
+                    /* <img class="w-100" src="<?php echo esc_url($image_url); ?>" alt="<?php echo $image_alt; ?>"> */
+                    ?>
+                            <div class="col-12 col-sm-<?php echo $sm ?> col-lg-<?php echo $lg; ?>">
+                                <a href="#" data-toggle="modal" data-target="#modal_<?php echo $i; ?>_<?php echo $id; ?>"><?php echo wp_get_attachment_image($image_id, 'full', "", array( "class" => "w-100" )) ?></a>
+                            </div>
+                            <div class="modal fade" id="modal_<?php echo $i; ?>_<?php echo $id; ?>" tabindex="-1" role="dialog" aria-label="<?php echo $image_alt; ?>" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img class="w-100" src="<?php echo esc_url($image_url); ?>" alt="<?php echo $image_alt; ?>">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-            <?php
-                $i++;
-            }
-            ?>
+                <?php
+                    $i++;
+                }
+                ?>
+            </div>
         </div>
     </div>
 </section>
