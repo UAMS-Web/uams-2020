2135cb1d18f9331e65aeea3a257e37eccc4b3593
todd-uams <todd@uams.edu>
2020-05-08 12:49:49 -0500

    Bug fix for broken pagination

--- lib/pagination.php
+++ lib/pagination.php
@@ -30,3 +30,21 @@ function uamswp_genesis_next_link_text_numeric( $text ) {
     }
     return $text;
 }
+// Pagination fix for /%category%/%postname%/
+function custom_pre_get_posts( $query ) {  
+    if( $query->is_main_query() && !$query->is_feed() && !is_admin() && is_category()) {  
+        $query->set( 'paged', str_replace( '/', '', get_query_var( 'page' ) ) );  
+    }  
+}
+add_action('pre_get_posts','custom_pre_get_posts'); 
+
+function custom_request($query_string ) { 
+        if( isset( $query_string['page'] ) ) { 
+            if( ''!=$query_string['page'] ) { 
+                if( isset( $query_string['name'] ) ) { 
+                    unset( $query_string['name'] ); } 
+            } 
+        } 
+    return $query_string; 
+}
+add_filter('request', 'custom_request');
