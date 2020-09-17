ff205be90dcb485362667f36aec2a0df767ef44b
joshdaugherty <joshdaugherty@gmail.com>
2020-09-17 11:03:11 -0500

    Disable nag updates for seopress & seopress-pro

--- lib/tgm.php
+++ lib/tgm.php
@@ -164,3 +164,21 @@ function uamswp_do_plugins_register() {
 	tgmpa( $plugins, $config );
 
 }
+// Disable Plugin Nags
+function disable_plugin_updates( $value ) {
+
+    $pluginsToDisable = [
+        'uamswp-seopress/seopress.php',
+        'uamswp-seopress-pro/seopress-pro.php'
+    ];
+
+    if ( isset($value) && is_object($value) ) {
+        foreach ($pluginsToDisable as $plugin) {
+            if ( isset( $value->response[$plugin] ) ) {
+                unset( $value->response[$plugin] );
+            }
+        }
+    }
+    return $value;
+}
+add_filter( 'site_transient_update_plugins', 'disable_plugin_updates' ); 
