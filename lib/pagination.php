7548cbea8f835b1e727303c5c063dd4e0889b7a2
joshdaugherty <joshdaugherty@gmail.com>
2020-09-17 10:54:39 -0500

    Fix pagination php error

--- lib/pagination.php
+++ lib/pagination.php
@@ -32,10 +32,12 @@ function uamswp_genesis_next_link_text_numeric( $text ) {
 }
 // Pagination fix for /%category%/%postname%/
 function remove_page_from_query_string($query_string) {
-	if ($query_string['name'] == 'page' && isset($query_string['page'])) {
-		unset($query_string['name']);
-		$query_string['paged'] = $query_string['page'];
-	}
+	if (isset($query_string['name'] ) ) {
+        if ($query_string['name'] == 'page' && isset($query_string['page'])) {
+            unset($query_string['name']);
+            $query_string['paged'] = $query_string['page'];
+        }
+    }
 	return $query_string;
 }
 add_filter('request', 'remove_page_from_query_string');
