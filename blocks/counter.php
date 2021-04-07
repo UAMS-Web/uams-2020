167412c4ea7f865137541e767f4b71b18b134440
todd-uams <todd@uams.edu>
2021-04-07 15:56:41 -0500

    Minor Fixes
    Fix JS
    Remove data-values for PHP values
    Add if statements for ACF values

--- blocks/counter.php
+++ blocks/counter.php
@@ -23,20 +23,25 @@ if( !empty($block['className']) ) {
 }
 if( !empty($block['align']) ) {
     $className .= ' align' . $block['align'];
-}
-    
+}   
 
 // Load values.
-$heading = get_field('counter_list_heading');
-$hide_heading = get_field('counter_list_hide_heading');
-$description = get_field('counter_list_description');
-$start = get_field('counter_list_start');
-$start_custom = get_field('counter_list_start_custom');
-$background_color = get_field('counter_list_background_color');
-$counter_list_rows = get_field('counter_list_section');
-$row = 0;
-date_default_timezone_set("America/Chicago");
+if ( empty($heading) ) 
+    $heading = get_field('counter_list_heading');
+if ( empty($hide_heading) ) 
+    $hide_heading = get_field('counter_list_hide_heading');
+if ( empty($description) ) 
+    $description = get_field('counter_list_description');
+if ( empty($start) ) 
+    $start = get_field('counter_list_start');
+if ( empty($start_custom) ) 
+    $start_custom = get_field('counter_list_start_custom');
+if ( empty($background_color) ) 
+    $background_color = get_field('counter_list_background_color');
+if ( empty($counter_list_rows) ) 
+    $counter_list_rows = get_field('counter_list_section');
 
+$row = 0;
 ?>
 <section class="uams-module link-list counter-list link-list-layout-split<?php echo $className; ?> <?php echo $background_color; ?>" id="<?php echo $id; ?>" aria-label="<?php echo $heading; ?>">
     <div class="container-fluid">
@@ -62,6 +67,7 @@ date_default_timezone_set("America/Chicago");
                         $counter_item_start_inherit = $counter_list_row['counter_list_section_start_inherit'];
                         $counter_item_start_override = $counter_list_row['counter_list_section_start_override'];
                         $counter_item_start_custom = $counter_list_row['counter_list_section_start_custom'];
+                        //
                         if ( $counter_item_start_inherit == 1 ) {
                             $counter_item_start = $start;
 
@@ -76,7 +82,7 @@ date_default_timezone_set("America/Chicago");
                             }
                         }
                         $date_day = date('Y-m-d') . ' 0:00:00';
-                        $date_week = date('Y-m-d',strtotime('last sunday')) . ' 0:00:00';
+                        $date_week = date('Y-m-d', strtotime('last sunday')) . ' 0:00:00';
                         $date_month = date('Y-m') . '-01 0:00:00';
                         $date_year = date('Y') . '-01-01 0:00:00';
                         $date_custom = $counter_item_start_custom;
@@ -99,7 +105,7 @@ date_default_timezone_set("America/Chicago");
                         }
 
                     ?>
-                        <li class="item" data-start-date="<?php echo $date_string; ?>" data-rate="<?php echo $counter_item_rate; ?>" data-time-elapsed="<?php echo $date_diff; ?>" data-current-count="<?php echo $count_value; ?>" data-unit-singular="<?php echo $counter_item_unit; ?>" data-unit-plural="<?php echo $counter_item_units; ?>" id="item-<?php echo $id . '-row-' . $row; ?>">
+                        <li class="item" data-start-date="<?php echo $date_string; ?>" data-rate="<?php echo $counter_item_rate; ?>" data-unit-singular="<?php echo $counter_item_unit; ?>" data-unit-plural="<?php echo $counter_item_units; ?>" id="item-<?php echo $id . '-row-' . $row; ?>">
                             <div class="text-container">
                                 <h3 class="h5"><?php echo $counter_item_title; ?></h3>
                                 <p class="count"><span class="value"><?php echo $count_value_display; ?></span> <span class="append"><?php echo $append; ?></span></p>
@@ -113,42 +119,42 @@ date_default_timezone_set("America/Chicago");
         </div>
     </div>
 </section>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
-	$(document).ready(function(){
-        $('.counter-list .item').each(function() {
-            var $id = $(this).attr('id');
-            var $this = $('#' + $id);
-            var $startDate = new Date($this.attr('data-start-date'));
-            var $dateDiff_php = $this.attr('data-time-elapsed');
-            var $rate = $this.attr('data-rate');
-            var $unit_singular = $this.attr('data-unit-singular');
-            var $unit_plural = $this.attr('data-unit-plural');
-            var $currentDate_js = new Date();
-            var $dateDiff_js = ($currentDate_js - $startDate) / 1000; // seconds
-            var $count = $dateDiff_js * $rate;
-
-            function recalculateCount() {
+    (function($) {
+        $(document).ready(function(){
+            $('.counter-list .item').each(function() {
+                var $id = $(this).attr('id');
+                var $this = $('#' + $id);
+                var $startDate = new Date($this.attr('data-start-date'));
+                var $rate = $this.attr('data-rate');
+                var $unit_singular = $this.attr('data-unit-singular');
+                var $unit_plural = $this.attr('data-unit-plural');
                 var $currentDate_js = new Date();
                 var $dateDiff_js = ($currentDate_js - $startDate) / 1000; // seconds
                 var $count = $dateDiff_js * $rate;
-                if ($count < 1) {
-                    var $count_display = '<1';
-                    var $unit_display = $unit_singular;
-                } else {
-                    var $count_display = Math.floor($dateDiff_js * $rate).toLocaleString('en');
-                    var $unit_display = $unit_plural;
+
+                function recalculateCount() {
+                    var $currentDate_js = new Date();
+                    var $dateDiff_js = ($currentDate_js - $startDate) / 1000; // seconds
+                    var $count = $dateDiff_js * $rate;
+                    if ($count < 1) {
+                        var $count_display = '<1';
+                        var $unit_display = $unit_singular;
+                    } else {
+                        var $count_display = Math.floor($dateDiff_js * $rate).toLocaleString('en');
+                        var $unit_display = $unit_plural;
+                    }
+                    $('#' + $id + ' .count .value').text($count_display);
+                    $('#' + $id + ' .count .append').text($unit_display);
                 }
-                $('#' + $id + ' .count .value').text($count_display);
-                $('#' + $id + ' .count .append').text($unit_display);
-            }
 
-            recalculateCount();
-
-            setInterval(function() {
                 recalculateCount();
-            }, 1000);
-        });
-	});
+
+                setInterval(function() {
+                    recalculateCount();
+                }, 1000);
+            });
+        })
+    })(jQuery);
 </script>
 
