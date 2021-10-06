9d97a9de308f1015e0e6bca7f63c0b6322663147
todd-uams <todd@uams.edu>
2021-10-06 14:32:38 -0500

    Add hide author and date to shortcode

--- blocks/news.php
+++ blocks/news.php
@@ -39,6 +39,10 @@ if ( empty($background_color) )
     $background_color = get_field('news_bgcolor');
 if ( empty($hide_img) )
     $hide_img = get_field('news_hide_img');
+if ( empty($hide_author) )
+    $hide_author = get_field('news_hide_author');
+if ( empty($hide_date) )
+    $hide_date = get_field('news_hide_date');
 if ( empty($articleID) )
     $articleID = get_field('news_article_id');
 if ( empty($local) )
@@ -62,7 +66,7 @@ if ( 'grid' == $output ) {
     $count = '1';
 }
 
-// echo '<!--[uamswp_news output="'. $output .'"  news_title="'. $title .'"  hide_title="'. $hide_title .'" category="'. $category .'" count="'. $count .'" offset="'. $offset .'" advanced_cat="'. $advancedCat .'" local="'. $local .'" style="'. $background_color . $className .'" hide_img="'. $hide_img .'" include_link="'. $link .'" news_position="'. $position .'" id="'. $articleID .'"]-->';
+// echo '<!--[uamswp_news output="'. $output .'"  news_title="'. $title .'"  hide_title="'. $hide_title .'" category="'. $category .'" count="'. $count .'" offset="'. $offset .'" advanced_cat="'. $advancedCat .'" local="'. $local .'" style="'. $background_color . $className .'" hide_img="'. $hide_img .'" hide_author="'. $hide_author.'" hide_date="'. $hide_date .'" include_link="'. $link .'" news_position="'. $position .'" id="'. $articleID .'"]-->';
 // echo '<!-- '; print_r($geo); echo ' -->';
 // GEO Logic
 $geo_display = false;
@@ -84,6 +88,6 @@ if (is_admin() && !empty($geo) && !empty($geo_region)) {
     echo ucwords($geo) . ' region(s): ' . implode(', ', $geo_region) . '<hr>';
 }
 if ($geo_display) {
-	echo do_shortcode('[uamswp_news output="'. $output .'"  news_title="'. $title .'"  hide_title="'. $hide_title .'" category="'. $category .'" count="'. $count .'" offset="'. $offset .'" advanced_cat="'. $advancedCat .'" local="'. $local .'" style="'. $background_color . $className .'" hide_img="'. $hide_img .'" include_link="'. $link .'" news_position="'. $position .'" id="'. $articleID .'"]' );
+	echo do_shortcode('[uamswp_news output="'. $output .'"  news_title="'. $title .'"  hide_title="'. $hide_title .'" category="'. $category .'" count="'. $count .'" offset="'. $offset .'" advanced_cat="'. $advancedCat .'" local="'. $local .'" style="'. $background_color . $className .'" hide_img="'. $hide_img .'" hide_author="'. $hide_author.'" hide_date="'. $hide_date .'" include_link="'. $link .'" news_position="'. $position .'" id="'. $articleID .'"]' );
 }
 ?>
