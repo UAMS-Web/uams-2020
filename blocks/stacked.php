b742fe05536f6b8ffc2399d5a3da4c1a9a435190
Josh <joshdaugherty@gmail.com>
2022-03-11 10:12:17 -0600

    [FD20-1534] Code refactoring, indentation

--- blocks/stacked.php
+++ blocks/stacked.php
@@ -99,57 +99,57 @@ if( $stacked_rows ) :
                 <?php echo $description ? '<div class="module-description"><p>'. $description .'</p></div>' : ''; ?>
             </div>
             <div class="col-12">
-            <div class="card-list card-list-left">
-            <?php 
-                foreach($stacked_rows as $stacked_row) {
-                // Load values.
-                $image = $stacked_row['stacked_section_image'];
-                $image_alt_native = get_post_meta($image, '_wp_attachment_image_alt', TRUE);
-                $image_alt_override = $stacked_row['stacked_section_alt_override'];
-                $item_heading = $stacked_row['stacked_section_heading'];
-                $body = $stacked_row['stacked_section_body'];
-                $button_text = $stacked_row['stacked_section_button_text'];
-                if ( $stacked_row['stacked_section_button_url'] ) {
-                    $button_url = $stacked_row['stacked_section_button_url']['url'];
-                    $button_target = $stacked_row['stacked_section_button_url']['target'];
-                }
-                $button_desc = $stacked_row['stacked_section_button_description'];
+                <div class="card-list card-list-left">
+                    <?php 
+                        foreach($stacked_rows as $stacked_row) {
+                        // Load values.
+                        $image = $stacked_row['stacked_section_image'];
+                        $image_alt_native = get_post_meta($image, '_wp_attachment_image_alt', TRUE);
+                        $image_alt_override = $stacked_row['stacked_section_alt_override'];
+                        $item_heading = $stacked_row['stacked_section_heading'];
+                        $body = $stacked_row['stacked_section_body'];
+                        $button_text = $stacked_row['stacked_section_button_text'];
+                        if ( $stacked_row['stacked_section_button_url'] ) {
+                            $button_url = $stacked_row['stacked_section_button_url']['url'];
+                            $button_target = $stacked_row['stacked_section_button_url']['target'];
+                        }
+                        $button_desc = $stacked_row['stacked_section_button_description'];
 
-            ?>
-            <div class="col-12 col-sm-6 col-xl-3 item">
-                <div class="card">
-                    <div class="card-img-top">
-                        <picture>
-                            <?php if ( function_exists( 'fly_add_image_size' ) ) { ?>  
-                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
-                                media="(min-width: 1921px)">
-                            <source srcset="<?php echo image_sizer($image, 433, 244, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 866, 487, 'center', 'center'); ?> 2x" 
-                                media="(min-width: 1500px)">
-                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
-                                media="(min-width: 992px)">
-                            <source srcset="<?php echo image_sizer($image, 433, 244, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 866, 487, 'center', 'center'); ?> 2x" 
-                                media="(min-width: 768px)">
-                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
-                                media="(min-width: 1px)">
-                            <!-- Fallback -->
-                            <img src="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?>" alt="<?php echo $image_alt_override ? $image_alt_override : $image_alt_native; ?>" />
-                            <?php } else { ?>
-                            <!-- Fallback -->
-                            <img src="<?php echo wp_get_attachment_image_url( $image, 'aspect-16-9' ); ?>" alt="<?php echo $image_alt_override ? $image_alt_override : $image_alt_native; ?>" />
-                            <?php } //endif ?>
-                        </picture>
-                    </div>
-                    <div class="card-body">
-                        <h3 class="card-title h5"><?php echo $item_heading; ?></h3>
-                        <p class="card-text"><?php echo $body; ?></p>
-                        <?php if ( $button_text ) { ?>  
-                        <a href="<?php echo $button_url; ?>" class="btn btn-primary stretched-link" aria-label="<?php echo $button_desc; ?>"<?php echo $button_target ? ' target="'. $button_target .'"' : ''; ?> data-moduletitle="<?php echo $heading; ?>" data-itemtitle="<?php echo $item_heading; ?>"><?php echo $button_text; ?></a>
-                        <?php } //endif ?>
-                    </div>
+                    ?>
+                        <div class="col-12 col-sm-6 col-xl-3 item">
+                            <div class="card">
+                                <div class="card-img-top">
+                                    <picture>
+                                        <?php if ( function_exists( 'fly_add_image_size' ) ) { ?>  
+                                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
+                                                media="(min-width: 1921px)">
+                                            <source srcset="<?php echo image_sizer($image, 433, 244, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 866, 487, 'center', 'center'); ?> 2x" 
+                                                media="(min-width: 1500px)">
+                                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
+                                                media="(min-width: 992px)">
+                                            <source srcset="<?php echo image_sizer($image, 433, 244, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 866, 487, 'center', 'center'); ?> 2x" 
+                                                media="(min-width: 768px)">
+                                            <source srcset="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?> 1x, <?php echo image_sizer($image, 910, 512, 'center', 'center'); ?> 2x" 
+                                                media="(min-width: 1px)">
+                                            <!-- Fallback -->
+                                            <img src="<?php echo image_sizer($image, 455, 256, 'center', 'center'); ?>" alt="<?php echo $image_alt_override ? $image_alt_override : $image_alt_native; ?>" />
+                                        <?php } else { ?>
+                                            <!-- Fallback -->
+                                            <img src="<?php echo wp_get_attachment_image_url( $image, 'aspect-16-9' ); ?>" alt="<?php echo $image_alt_override ? $image_alt_override : $image_alt_native; ?>" />
+                                        <?php } //endif ?>
+                                    </picture>
+                                </div>
+                                <div class="card-body">
+                                    <h3 class="card-title h5"><?php echo $item_heading; ?></h3>
+                                    <p class="card-text"><?php echo $body; ?></p>
+                                    <?php if ( $button_text ) { ?>  
+                                        <a href="<?php echo $button_url; ?>" class="btn btn-primary stretched-link" aria-label="<?php echo $button_desc; ?>"<?php echo $button_target ? ' target="'. $button_target .'"' : ''; ?> data-moduletitle="<?php echo $heading; ?>" data-itemtitle="<?php echo $item_heading; ?>"><?php echo $button_text; ?></a>
+                                    <?php } //endif ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } // end foreach ?>
                 </div>
-            </div>
-            <?php } // end foreach ?>
-            </div>
             </div>
             <?php if ( $more ) { ?>
                 <div class="col-12 more">
