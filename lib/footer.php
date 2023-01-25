aa8ac93b4c022a07433d9048c136f9b92e3deedd
joshdaugherty <joshdaugherty@gmail.com>
2023-01-25 11:22:36 -0600

    [FD20-3127] Set footer legal links to open in new window

--- lib/footer.php
+++ lib/footer.php
@@ -344,11 +344,11 @@ function uamswp_footer_creds_text() {
     // Text Links
     // Render this by default
     echo '<div role="navigation" aria-label="Footer Navigation"><ul class="nav legal">';
-        echo '<li class="nav-item"><a class="nav-link" href="https://uamshealth.com/disclaimer/">Disclaimer</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="https://uamshealth.com/terms-of-use/">Terms of Use</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" href="https://web.uams.edu/privacy/">Privacy Statement</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="https://uamshealth.com/disclaimer/" target="_blank">Disclaimer</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="https://uamshealth.com/terms-of-use/" target="_blank">Terms of Use</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="https://web.uams.edu/privacy/" target="_blank">Privacy Statement</a></li>';
         if ('uamshealth' == $site) { // If UAMS Health site, add additional links
-            echo '<li class="nav-item"><a class="nav-link" href="https://hipaa.uams.edu/forms/notice-of-privacy-practices-information/">Notice of Privacy Practices</a></li>';
+            echo '<li class="nav-item"><a class="nav-link" href="https://hipaa.uams.edu/forms/notice-of-privacy-practices-information/" target="_blank">Notice of Privacy Practices</a></li>';
         }
         //echo '<li class="nav-item"><a class="nav-link" href="/sitemap">Site Map</a></li>';
     echo '</ul></div>';
