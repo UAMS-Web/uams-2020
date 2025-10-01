406c28c598fe3dbba19e21fb69a7a26777ec1355
todd-uams <todd@uams.edu>
2025-10-01 10:04:00 -0500

    Validate $block['atts']

--- lib/media-credit.php
+++ lib/media-credit.php
@@ -337,7 +337,7 @@
 
 				// Fallback: Retrieve the credit value from the asset file's image metadata
 
-					if ( !isset($photo_credit) ) {
+					if ( !isset($photo_credit) && is_array($block['attrs']) ) {
 
 						$photo_credit = wp_get_attachment_metadata(
 							$block['attrs']['id'] // int // required // Attachment post ID. Defaults to global $post.
